c757c079d80bc8bedfe397e2f1f1732005694c06
Shane Rosenthal <srosenthal82@gmail.com>
2026-09-18 12:06:27 -0400

    u

--- resources/views/native/home.blade.php
+++ resources/views/native/home.blade.php
@@ -23,8 +23,8 @@
                 <row class="w-full items-center gap-3">
                     <pressable class="flex-1" @press="checkForOtaUpdate">
                         <text
-                            class="px-4 py-3 bg-theme-surface-variant rounded-xl border border-theme-outline text-sm font-semibold text-theme-on-surface text-center">
-                            Checkasdasdasdasdasdasd
+                            class="px-4 py-3 bg-theme-surface-variant bg-yellow-400 rounded-xl border border-theme-outline text-sm font-semibold text-theme-on-surface text-center">
+                            Check
                         </text>
                     </pressable>
                     <pressable class="flex-1" @press="downloadOtaUpdate">
