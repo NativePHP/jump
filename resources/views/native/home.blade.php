f5bcbbb348cd885fbd1dcdf99a336aa32a5f51a2
Shane Rosenthal <srosenthal82@gmail.com>
2026-09-18 18:08:00 -0400

    u

--- resources/views/native/home.blade.php
+++ resources/views/native/home.blade.php
@@ -24,7 +24,7 @@
                     <pressable class="flex-1 " @press="checkForOtaUpdate">
                         <text
                             class="px-4 py-3 bg-theme-surface-variant rounded-xl border border-theme-outline text-sm font-semibold text-theme-on-surface text-center">
-                            Check
+                            Checking android ota
                         </text>
                     </pressable>
                     <pressable class="flex-1" @press="downloadOtaUpdate">
